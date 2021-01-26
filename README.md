c5cdce7e9c6335997d534a84abfb55e5f1d81be4
Christoph Haas <christoph.h@sprinternet.at>
2021-01-26 22:02:09 +0100

    WIP: 8.x.x

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 
 ## About
 
-- This repository contains the sources that are used to build the `h44z/seafile-ce` docker image.
+- This repository contains the sources that are used to build the `h44z/seafile-ce` docker image. Currently tested with Seafile CE 8.0.2.
 
-- It is based on the official docker image (https://github.com/haiwen/seafile-docker). 
+- The main goal of this image is to provide a really simple and clean docker image for Seafile Community Edition.
+ The official docker image is quite complex and hard to extend or modify. This image instead provides a simple way to deploy a standardized Seafile instance with Docker.
 
-- The image only contains the community edition of Seafile. The build process has been simplified and improved in comparison to the official image.
+- Automated features that come with this Docker image:
+  - newest seafile version at rebuild
+  - Automatically runs upgrade scripts (when pulling a newer image) provided by seafile
+  - Configurable to run with MySQL/MariaDB or SQLite
+  - Auto-setup at initial run
+  - Supports different startup mode (MODE):
+    - autorun: Default mode, setups server (if neccessary), upgrade database, then run server.
+    - collect_garbage: Run garbage collector instead of starting server
+    - diagnose: Run Seafile fsck to validate file consistency.
+    - maintenance: Only start the container. You can log into it with docker exec and work on it.
 
-- It is possible to use this image with an existing, none dockerized Seafile installation. The usage of an external reverse proxy (like nginx) is also supported.
-
-- Setting up is really easy and can be (or should be) done via Docker Compose.
-
+- It is largly based on this docker image: https://github.com/Gronis/docker-seafile. 
 
 ## Building the docker image from scratch
 To build the docker image, docker must be installed (at least version 18.02.0). 
@@ -20,30 +27,57 @@ After the dependcies have been set up, change to the image directory and build t
 ```
 # get the sources
 git clone https://github.com/h44z/seafile-docker.git
-cd seafile-docker/image
+cd seafile-docker
+cp .env.dist .env
+edit .env  # if needed (for building, only the version is needed)
 
 # build the image
-make base
+cd image
 make server
 ```
 
-## Running Seafile 7.x.x with docker-compose
-Make sure that you have installed Docker Compose with version 1.21.0 or higher.
 
-```
-# get the sources
-git clone https://github.com/h44z/seafile-docker.git
-cd seafile-docker
+## Running Seafile 8.x.x with docker-compose
+Make sure that you have installed Docker Compose with version 1.21.0 or higher. Setting up Seafile is really easy and can be (or should be) done via Docker Compose. All important data is stored under `/seafile` so you should be mounting a volume there (recommended), as shown in the example configurations, or at the respective subdirectories.
 
-# configure the environment, change seafile settings
+The first step is to create a `.env` file by copying the provided .env.dist file:
+```bash
 cp .env.dist .env
-edit .env
-
-# start it!
-docker-compose up
-
-# have fun...
 ```
+**Mandatory ENV variables for auto setup**
+
+* **SEAFILE_NAME**: Name of your Seafile installation
+* **SEAFILE_ADDRESS**: URL to your Seafile installation
+* **SEAFILE_ADMIN**: E-mail address of the Seafile admin
+* **SEAFILE_ADMIN_PW**: Password of the Seafile admin
+
+If you want to use MySQL/MariaDB, the following variables are needed:
+
+**Mandatory ENV variables for MySQL/MariaDB**
+* **MYSQL_SERVER**: Address of your MySQL server
+* **MYSQL_USER**: MySQL user Seafile should use
+* **MYSQL_USER_PASSWORD**: Password for said MySQL User
+* **MYSQL_PORT**: Port MySQL runs on (Optional, default 3306)
+
+**Optional ENV variables for auto setup with MySQL/MariaDB**
+* **MYSQL_USER_HOST**: Host the MySQL User is allowed from (default: '%')
+* **MYSQL_ROOT_PASSWORD**: If you haven't set up the MySQL tables by yourself, Seafile will do it for you when being provided with the MySQL root password
+
+Using the `MODE` environment variable, the startup behaviour of the container can be managed, defaults to *autorun*.
+
+A sample docker-compose file is provided within this repository.
+
+
+## Upgrading from Seafile 7.x.x
+This version of the image is designed to work with version 8.x.x.
+The previous version of this Docker image was based on the official Docker image, thus a few changes have to be made in order to upgrade to the new version.
+
+Changes:
+ - Nginx and Letsencrypt are no longer included in the image.
+ - The log directory is no longer included in the main directory that can be exported using volumes.
+ - The path of the main directory changed from `/shared` to `/seafile`.
+ - The file holding the current seafile version now lives in `/seafile/current_version` instead of `/shared/seafile-data/current_version`.
+ - Many environment variables have been removed in order to keep this image and the setup script simple. Special customizations can still be achived, see [here](#manual-configuration-of-seafile).
 
 
 ### Environment Variables
@@ -51,7 +85,7 @@ Take a look at .env.dist for all available environment variables. Copy `.env.dis
 
 
 ### Manual configuration of Seafile
-After the Seafile container has been started at least once, the mounted volumes should contain a folder `shared`. The seafile configuration files are located in `shared/seafile/conf`. Take a look at the official manual to check out which settings can be changed.
+After the Seafile container has been started at least once, the mounted volume for the `/seafile` directory should contain a folder `conf`. Take a look at the official manual to check out which settings can be changed.
 
 HINT: After the initial setup (first run), changing the environment variables in .env does not reflect to the configuration files!
 
---
+++ image/seafile/scripts/seafile-entrypoint.sh
@@ -0,0 +1,415 @@
+#!/bin/bash
+
+DATADIR=${DATADIR:-"/seafile"}
+BASEPATH=${BASEPATH:-"/opt/seafile"}
+INSTALLPATH=${INSTALLPATH:-"${BASEPATH}/$(ls -1 ${BASEPATH} | grep -E '^seafile-server-[0-9.-]+')"}
+VERSION=$(echo $INSTALLPATH | grep -oE [0-9.]+)
+OLD_VERSION=$(cat $DATADIR/current_version 2> /dev/null || echo $VERSION)
+MAJOR_VERSION=$(echo $VERSION | cut -d. -f 1-2)
+OLD_MAJOR_VERSION=$(echo $OLD_VERSION | cut -d. -f 1-2)
+VIRTUAL_PORT=${VIRTUAL_PORT:-"8000"}
+
+python3 --version
+uname -a
+echo "Current seafile version: $VERSION ($MAJOR_VERSION)"
+echo "Old seafile version: $OLD_VERSION ($OLD_MAJOR_VERSION)"
+
+
+set -e
+set -u
+set -o pipefail
+
+trapped() {
+  control_seahub "stop"
+  control_seafile "stop"
+}
+
+handle_error() {
+  if [ "$1" != "0" ]; then
+    # error handling goes here
+    echo "Unclean shutdown: $1 , occurred on line $2"
+
+    # Try to shutdown everything
+    control_seahub "stop"
+    control_seafile "stop"
+  fi
+}
+
+autorun() {
+  # Update if neccessary
+  if [ $OLD_VERSION != $VERSION ]; then
+    full_update
+  fi
+  echo $VERSION > $DATADIR/current_version
+  # Needed to check the return code
+  set +e
+  control_seafile "start"
+  local RET=$?
+  set -e
+  # Try an initial setup on error
+  if [ ${RET} -eq 255 ]
+  then
+    choose_setup
+    control_seafile "start"
+  elif [ ${RET} -gt 0 ]
+  then
+    exit 1
+  fi
+
+  fix_gunicorn_config
+
+  if [ ${SEAFILE_FASTCGI:-} ]
+  then
+    control_seahub "start-fastcgi"
+  else
+    control_seahub "start"
+  fi
+  keep_in_foreground
+}
+
+run_only() {
+  local SH_DB_DIR="${DATADIR}/${SEAHUB_DB_DIR}"
+  control_seafile "start"
+  control_seahub "start"
+  keep_in_foreground
+}
+
+choose_setup() {
+  set +u
+  # If $MYSQL_SERVER is set, we assume MYSQL setup is intended,
+  # otherwise sqlite
+  if [ -n "${MYSQL_SERVER}" ]
+  then
+    set -u
+    setup_mysql
+  else
+    set -u
+    setup_sqlite
+  fi
+
+}
+
+setup_mysql() {
+  echo "setup_mysql"
+
+  # Wait for MySQL to boot up
+  DOCKERIZE_TIMEOUT=${DOCKERIZE_TIMEOUT:-"60s"}
+  dockerize -timeout ${DOCKERIZE_TIMEOUT} -wait tcp://${MYSQL_SERVER}:${MYSQL_PORT:-3306}
+
+  set +u
+  OPTIONAL_PARMS="$([ -n "${MYSQL_ROOT_PASSWORD}" ] && printf '%s' "-r ${MYSQL_ROOT_PASSWORD}")"
+  set -u
+
+  . /tmp/seafile.env; ${INSTALLPATH}/setup-seafile-mysql.sh auto \
+    -n "${SEAFILE_NAME}" \
+    -i "${SEAFILE_ADDRESS}" \
+    -p "${SEAFILE_PORT}" \
+    -d "${SEAFILE_DATA_DIR}" \
+    -o "${MYSQL_SERVER}" \
+    -t "${MYSQL_PORT:-3306}" \
+    -u "${MYSQL_USER}" \
+    -w "${MYSQL_USER_PASSWORD}" \
+    -q "${MYSQL_USER_HOST:-"%"}" \
+    ${OPTIONAL_PARMS}
+
+  setup_seahub
+  move_and_link
+}
+
+setup_sqlite() {
+  echo "setup_sqlite"
+  # Setup Seafile
+  . /tmp/seafile.env; ${INSTALLPATH}/setup-seafile.sh auto \
+    -n "${SEAFILE_NAME}" \
+    -i "${SEAFILE_ADDRESS}" \
+    -p "${SEAFILE_PORT}" \
+    -d "${SEAFILE_DATA_DIR}"
+
+  setup_seahub
+  move_and_link
+}
+
+setup_seahub() {
+  # Setup Seahub
+
+  # From https://github.com/haiwen/seafile-server-installer-cn/blob/master/seafile-server-ubuntu-14-04-amd64-http
+  sed -i 's/= ask_admin_email()/= '"\"${SEAFILE_ADMIN}\""'/' ${INSTALLPATH}/check_init_admin.py
+  sed -i 's/= ask_admin_password()/= '"\"${SEAFILE_ADMIN_PW}\""'/' ${INSTALLPATH}/check_init_admin.py
+
+  if [ "${SPRINTERNET_CUSTOMIZATIONS:-false}" = "true" ]; then
+    echo Patching files...
+    sed -i "/{% block main_content %}/a <script type=\"text/javascript\">window.location.replace(\"{% url 'oauth_login' %}\");</script>" ${INSTALLPATH}/seahub/seahub/templates/registration/login.html
+    sed -i '/<\/div>/a <script type="text/javascript">window.location.replace("https://account.sprinternet.at/ssologout?src=seafile");</script>' ${INSTALLPATH}/seahub/seahub/templates/registration/logout.html
+    echo Patching completed...
+  fi
+
+  control_seafile "start"
+
+  . /tmp/seafile.env; python3 -t ${INSTALLPATH}/check_init_admin.py
+}
+
+move_and_link() {
+  # As seahub.db is normally in the root dir of seafile (/opt/seafile)
+  # SEAHUB_DB_DIR needs to be defined if it should be moved elsewhere under /seafile
+  local SH_DB_DIR="${DATADIR}/${SEAHUB_DB_DIR}"
+  # Stop Seafile/hub instances if running
+  control_seahub "stop"
+  control_seafile "stop"
+
+  move_files "${SH_DB_DIR}"
+  link_files "${SH_DB_DIR}"
+
+  if [ ! -w ${DATADIR} ]; then
+    echo "Updating file permissions"
+    chown -R root:root ${DATADIR}/
+  fi
+}
+
+fix_gunicorn_config() {
+  echo "Fixing gunicorn config."
+  # Must bind 0.0.0.0 instead of 127.0.0.1
+  CONFIG_FILE=/seafile/conf/gunicorn.conf.py
+  OLD="bind = \"127.0.0.1:${VIRTUAL_PORT}\""
+  NEW="bind = \"0.0.0.0:${VIRTUAL_PORT}\""
+  sed -i "s/${OLD}/${NEW}/g" $CONFIG_FILE
+}
+
+move_files() {
+  MOVE_LIST=(
+    "${BASEPATH}/ccnet:${DATADIR}/ccnet"
+    "${BASEPATH}/conf:${DATADIR}/conf"
+    "${BASEPATH}/seafile-data:${DATADIR}/seafile-data"
+    "${BASEPATH}/seahub-data:${DATADIR}/seahub-data"
+    "${INSTALLPATH}/seahub/media/avatars:${DATADIR}/seahub-data/avatars"
+    "${INSTALLPATH}/seahub/media/custom:${DATADIR}/seahub-data/custom"
+  )
+  for SEADIR in ${MOVE_LIST[@]}
+  do
+    ARGS=($(echo $SEADIR | tr ":" "\n"))
+    if [ -e "${ARGS[0]}" ] && [ ! -e "${ARGS[1]}" ]
+    then
+      echo "Copying ${ARGS[0]} => ${ARGS[1]}"
+      local PARENT=$(dirname ${ARGS[1]})
+      if [ ! -e $PARENT ]
+      then
+        mkdir -p $PARENT
+      fi
+      cp -a ${ARGS[0]}/ ${ARGS[1]}
+    fi
+    if [ -e "${ARGS[0]}" ] && [ ! -L "${ARGS[0]}" ]
+    then
+      echo "Dropping ${ARGS[0]}"
+      rm -rf ${ARGS[0]}
+    fi
+  done
+
+  if [ -e "${BASEPATH}/seahub.db" -a ! -L "${BASEPATH}/seahub.db" ]
+  then
+    mv ${BASEPATH}/seahub.db ${1}/
+  fi
+}
+
+link_files() {
+  LINK_LIST=(
+    "${DATADIR}/ccnet:${BASEPATH}/ccnet"
+    "${DATADIR}/conf:${BASEPATH}/conf"
+    "${DATADIR}/seafile-data:${BASEPATH}/seafile-data"
+    "${DATADIR}/seahub-data:${BASEPATH}/seahub-data"
+    "${DATADIR}/seahub-data/avatars:${INSTALLPATH}/seahub/media/avatars"
+    "${DATADIR}/seahub-data/custom:${INSTALLPATH}/seahub/media/custom"
+
+  )
+  for SEADIR in ${LINK_LIST[@]}
+  do
+    ARGS=($(echo $SEADIR | tr ":" "\n"))
+    if [ -e "${ARGS[0]}" ]
+    then
+      echo "Linking ${ARGS[1]} => ${ARGS[0]}"
+      ln -sf ${ARGS[0]} ${ARGS[1]}
+    fi
+  done
+  if [ -e "${SH_DB_DIR}/seahub.db" -a ! -L "${BASEPATH}/seahub.db" ]
+  then
+    ln -s ${1}/seahub.db ${BASEPATH}/seahub.db
+  fi
+
+}
+
+keep_in_foreground() {
+  echo "Running main progress in foreground now..."
+  # As there seems to be no way to let Seafile processes run in the foreground we
+  # need a foreground process. This has a dual use as a supervisor script because
+  # as soon as one process is not running, the command returns an exit code >0
+  # leading to a script abortion thanks to "set -e".
+  while true
+  do
+    for SEAFILE_PROC in "seafile-control" "seaf-server" "gunicorn"
+    do
+      pkill -0 -f "${SEAFILE_PROC}"
+      sleep 1
+    done
+    sleep 5
+  done
+}
+
+prepare_env() {
+  cat << _EOF_ > /tmp/seafile.env
+  export LANG='en_US.UTF-8'
+  export LC_ALL='en_US.UTF-8'
+  export CCNET_CONF_DIR="${BASEPATH}/ccnet"
+  export SEAFILE_CONF_DIR="${SEAFILE_DATA_DIR}"
+  export SEAFILE_CENTRAL_CONF_DIR="${BASEPATH}/conf"
+  export PYTHONPATH=${INSTALLPATH}/seafile/lib/python3.6/site-packages:${INSTALLPATH}/seafile/lib64/python3.6/site-packages:${INSTALLPATH}/seahub:${INSTALLPATH}/seahub/thirdpart:${INSTALLPATH}/seafile/lib/python3.6/site-packages:${INSTALLPATH}/seafile/lib64/python3.6/site-packages:${PYTHONPATH:-}
+
+_EOF_
+}
+
+control_seafile() {
+  . /tmp/seafile.env; ${INSTALLPATH}/seafile.sh "$@"
+  local RET=$?
+  if [ $RET -gt 0 ]; then
+    print_log
+  fi
+  return ${RET}
+}
+
+control_seahub() {
+  . /tmp/seafile.env; ${INSTALLPATH}/seahub.sh "$@"
+  local RET=$?
+  if [ $RET -gt 0 ]; then
+    print_log
+  fi
+  return ${RET}
+}
+
+print_log() {
+    if [ -e $INSTALLPATH/../logs ]
+    then
+      sleep 2
+      echo ""
+      echo "---------------------------------------"
+      echo "seafile.log:"
+      echo "---------------------------------------"
+      cat $INSTALLPATH/../logs/seafile.log
+      echo ""
+      echo "---------------------------------------"
+      echo "controller.log:"
+      echo "---------------------------------------"
+      cat $INSTALLPATH/../logs/controller.log
+      echo ""
+      echo "---------------------------------------"
+      echo "ccnet.log:"
+      echo "---------------------------------------"
+      cat $INSTALLPATH/../logs/ccnet.log
+    fi
+}
+
+full_update(){
+  EXECUTE=""
+  echo ""
+  echo "---------------------------------------"
+  echo "Upgrading from $OLD_VERSION to $VERSION"
+  echo "---------------------------------------"
+  echo ""
+  # Iterate through all the major upgrade scripts and apply them
+  for i in `ls ${INSTALLPATH}/upgrade/`; do
+    if [ `echo $i | grep "upgrade_${OLD_MAJOR_VERSION}"` ]; then
+      EXECUTE=1
+    fi
+    if [ $EXECUTE ] && [ `echo $i | grep upgrade` ]; then
+      echo "Running update $i"
+      update $i || exit
+    fi
+  done
+  # When all the major upgrades are done, perform a minor upgrade
+  if [ -z $EXECUTE ]; then
+    echo "Running minor upgrade"
+    update minor-upgrade.sh
+    echo $VERSION > $DATADIR/current_version
+  fi
+}
+
+update(){
+  . /tmp/seafile.env; echo -ne '\n' | ${INSTALLPATH}/upgrade/$@
+  local RET=$?
+  sleep 1
+  return ${RET}
+}
+
+collect_garbage(){
+  . /tmp/seafile.env; ${INSTALLPATH}/seaf-gc.sh $@
+  local RET=$?
+  sleep 1
+  return ${RET}
+}
+
+diagnose(){
+  . /tmp/seafile.env; ${INSTALLPATH}/seaf-fsck.sh $@
+  local RET=$?
+  sleep 1
+  return ${RET}
+}
+
+maintenance(){
+  local SH_DB_DIR="${DATADIR}/${SEAHUB_DB_DIR}"
+  # Linking must always be done
+  link_files "${SH_DB_DIR}"
+  echo ""
+  echo "---------------------------------------"
+  echo "Running in maintenance mode"
+  echo "---------------------------------------"
+  echo ""
+  tail -f /dev/null
+}
+
+
+# Fill vars with defaults if empty
+if [ -z ${MODE+x} ]; then
+  MODE=${1:-"run"}
+fi
+
+SEAFILE_DATA_DIR=${SEAFILE_DATA_DIR:-"${DATADIR}/seafile-data"}
+SEAFILE_PORT=${SEAFILE_PORT:-8082}
+SEAHUB_DB_DIR=${SEAHUB_DB_DIR:-}
+
+prepare_env
+
+trap trapped SIGINT SIGTERM
+trap 'handle_error $? $LINENO' EXIT
+
+move_and_link
+case $MODE in
+  "autorun" | "run")
+    autorun
+  ;;
+  "setup" | "setup_mysql")
+    setup_mysql
+  ;;
+  "setup_sqlite")
+    setup_sqlite
+  ;;
+  "setup_seahub")
+    setup_seahub
+  ;;
+  "setup_only")
+    choose_setup
+  ;;
+  "run_only")
+    run_only
+  ;;
+  "update")
+    full_update
+  ;;
+  "diagnose")
+    diagnose
+  ;;
+  "collect_garbage")
+    collect_garbage
+  ;;
+  "maintenance")
+    maintenance
+  ;;
+  "stop")
+    trapped
+  ;;
+esac
