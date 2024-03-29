e8566325b6600817b81ec84ee73658e5ed2a3417
root <root@srv3.sprinternet.at>
2024-03-29 15:14:23 +0100

    minor fixes

--- backup-db.sh
+++ backup-db.sh
@@ -6,8 +6,11 @@ cd $SCRIPT_DIR
 set -a; source .env; set +a
 
 mkdir -p dbbackup
-mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines ccnet_db > dbbackup/ccnet.sql
-mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines seafile_db > dbbackup/seafile.sql
-mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines seahub_db > dbbackup/seahub.sql
+
+echo "Performing backup..."
+
+docker compose exec db mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines ccnet_db > dbbackup/ccnet.sql
+docker compose exec db mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines seafile_db > dbbackup/seafile.sql
+docker compose exec db mysqldump -u root --password=$MYSQL_ROOT_PASSWORD --single-transaction --routines seahub_db > dbbackup/seahub.sql
 
 echo "DB backup finished - check output"
